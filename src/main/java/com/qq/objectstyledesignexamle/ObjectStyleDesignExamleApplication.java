67b1a15a9db3bb77ca44a42f6d83ae9a8aa2ab4e
kgh2120 <kgh2120@gmail.com>
2025-12-29 22:55:36 +0900

    init pjt

---
+++ src/main/java/com/qq/objectstyledesignexamle/ObjectStyleDesignExamleApplication.java
@@ -0,0 +1,13 @@
+package com.qq.objectstyledesignexamle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObjectStyleDesignExamleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ObjectStyleDesignExamleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/qq/objectstyledesignexamle/ObjectStyleDesignExamleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qq.objectstyledesignexamle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObjectStyleDesignExamleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
